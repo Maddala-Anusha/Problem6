0869ba8b269abdb37a6795f9da5927599c44fff5
Anusha <2100031469@kluniversity.in>
2024-06-15 12:56:17 +0530

    project

---
+++ JP6/src/main/java/com/example/JPProblem6/JpProblem6Application.java
@@ -0,0 +1,13 @@
+package com.example.JPProblem6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpProblem6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpProblem6Application.class, args);
+	}
+
+}
